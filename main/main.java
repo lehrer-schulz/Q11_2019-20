ae3f43ecb75b3b96b233b0a09b71d641545b7045
chrismeister02 <chrismeister02>
2020-03-24 02:11:50 +0100

    Bug-Fixing

---
+++ main/main.java
@@ -0,0 +1,17 @@
+package main;
+
+public class main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Halo, I bims nochmal");
+        //Comment with no further meaning
+        Hauptmenu form = new Hauptmenu();
+        form.setVisible(true);
+    }
+
+    public void foo()
+    {
+        System.out.println("Nothing to do here.");
+    }
+}
